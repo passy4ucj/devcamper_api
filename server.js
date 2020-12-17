4b9502d1d8537226059f4174260df7d6df9babf4
passy4ucj <ojinnnakapascal@gmail.com>
2020-12-17 14:56:48 +0100

    COlors in the console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv  from 'dotenv'
 import logger from './middleware/logger.js'
 import morgan from 'morgan'
+import colors from 'colors'
 import connectDB from './config/db.js'
 
 
@@ -34,4 +35,4 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
